84efb2e673784d6c4b80247a80159503f1235029
Max Kuo <maxkuo712@gmail.com>
2022-06-02 10:25:50 +0800

    20220602 update @CrossOrigin

--- orderhippo/src/main/java/com/orderhippo/controller/viewController/VMonthReveueController.java
+++ orderhippo/src/main/java/com/orderhippo/controller/viewController/VMonthReveueController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,7 @@ import io.swagger.annotations.ApiOperation;
 @Api(tags = "每月營收API")
 @RestController
 @RequestMapping("/api")
-
+@CrossOrigin
 public class VMonthReveueController {
 	
 	@Autowired
--- orderhippo/src/main/java/com/orderhippo/controller/viewController/VPaymentDetailController.java
+++ orderhippo/src/main/java/com/orderhippo/controller/viewController/VPaymentDetailController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import io.swagger.annotations.Api;
 @Api(tags = "更改菜單顯示API")
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class VPaymentDetailController {
 	
 	@Autowired
