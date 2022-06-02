ffbb5e4ab7d75208fff1d155cda89745ec070574
Max Kuo <maxkuo712@gmail.com>
2022-06-03 02:11:01 +0800

    20220603 update main.js

--- 後台登入系統/js/main.js
+++ 後台登入系統/js/main.js
@@ -1,33 +1,34 @@
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
 import {
     getAuth,
-    signOut
+    signOut,
+    signInWithEmailAndPassword,
+    GoogleAuthProvider,
+    signInWithPopup,
+    sendPasswordResetEmail
 } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-auth.js";
 
-import { myFirebase } from "./firebase.js";
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyBqQ5T0uy3_68BVFhfTqS98VNWUmgLkir0",
+    authDomain: "orderhippo-store.firebaseapp.com",
+    projectId: "orderhippo-store",
+    storageBucket: "orderhippo-store.appspot.com",
+    messagingSenderId: "777247881627",
+    appId: "1:777247881627:web:6bc31c0a8fd80a4def493e"
+};
 
-// // Initialize Firebase
-const myLoginFirebase = new myFirebase();
-myLoginFirebase.initFirebase();
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDmqTvgYkvxv42UFqnHl8ejXg627WUow1k",
-//     authDomain: "login-test-18ab1.firebaseapp.com",
-//     projectId: "login-test-18ab1",
-//     storageBucket: "login-test-18ab1.appspot.com",
-//     messagingSenderId: "521236544499",
-//     appId: "1:521236544499:web:be27cbcb82b1d84f09f086",
-//     measurementId: "G-4QG29VS5S8",
-// };
 
 // Initialize Firebase
-// initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 let signBnt = document.getElementById("signBnt");
 
 // setSignBntStatus
 setSignBntStatus();
 
-function userSignOut() {
+// 登出
+function storeSignOut() {
     const auth = getAuth();
     signOut(auth).then(() => {
         signOutOK();
@@ -48,25 +49,17 @@ function signOutOK() {
 function setSignBntStatus() {
     const auth = getAuth();
     const signBnt = document.getElementById("signBnt");
-    auth.onAuthStateChanged((user) =>{
-        if (user) {
-            // signBnt.innerText = "Sign out";
+    auth.onAuthStateChanged((store) =>{
+        if (store) { // 有登入
             // window.location.href = "homepage.html";
-        } else {
-            // signBnt.innerText = "Sign in";
-            // window.location.href = "homepage.html";
+        } else { // 沒登入
+            window.location.href = "login.html";
         }
     });
 }
 
 signBnt.addEventListener("click", (e) => {
-    // let signText = signBnt.innerText;
-    // if (signText === "Sign in") {
-    //     window.location.href = "login.html";
-    // } else if (signText === "Sign out") {
-
-    // }
-    userSignOut();
+    storeSignOut();
     localStorage.removeItem("storeinfo");
     localStorage.removeItem("storeToken");
     window.location.href = "login.html";
