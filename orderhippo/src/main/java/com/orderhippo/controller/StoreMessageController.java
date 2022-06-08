ffb7f079548cf2e880621a1645be174a07acc206
Max Kuo <maxkuo712@gmail.com>
2022-06-08 11:05:27 +0800

    20220607 update StoreMessage

--- orderhippo/src/main/java/com/orderhippo/controller/StoreMessageController.java
+++ orderhippo/src/main/java/com/orderhippo/controller/StoreMessageController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,7 +30,7 @@ import io.swagger.annotations.ApiOperation;
 @Api(tags = "店家公告訊息API")
 @RestController
 @RequestMapping("/api")
-
+@CrossOrigin
 public class StoreMessageController {
 	
 	@Autowired
